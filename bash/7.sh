149ae4757237bd8ab3f54aa5145fb408bb2242ca
Ivan Chernomorov <90576997+IvanChernomorov@users.noreply.github.com>
2023-11-10 17:29:37 +0300

    Залил 7 задание по bash

---
+++ bash/7.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IFS=':' read -ra paths <<< "$PATH"
+for path in "${paths[@]}"; do
+    if [ -d "$path" ]; then
+        num_files=$(find "$path" -maxdepth 1 -type f | wc -l)
+        echo "$path => $num_files"
+    fi
+done
